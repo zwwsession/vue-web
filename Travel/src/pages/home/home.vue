b0b28a0ec1ed4161bbc9e60ad33b928d2e04640c
zww <zwwtnt@yeah.net>
2018-05-01 20:46:38 +0800

    初始化项目

---
+++ Travel/src/pages/home/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="home">hello word</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style scoped>
+  .home {
+    font-size: 50px;
+  }
+</style>
